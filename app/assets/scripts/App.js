a0f4246310ce1e021ebd759698ddda29e7becf31
Noxemper <vanmechelenjonas@hotmail.com>
2017-09-24 13:37:43 +0200

    Completed overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import $ from "jquery";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 var mobileMenu = new MobileMenu();
 var revealFeatureItems = new RevealOnScroll($(".feature-item"), "85%");
 var revealTestimonials = new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
